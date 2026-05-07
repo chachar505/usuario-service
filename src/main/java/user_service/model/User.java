94c2bad64e38eee6ea7bea4bc43ea7d97fc484ee
hxllw-bxdies <ric.leonc@duocuc.cl>
2026-05-07 16:05:13 -0400

    model creado, conectado a la base de datos y flyway activado

---
+++ src/main/java/user_service/model/User.java
@@ -0,0 +1,69 @@
+package user_service.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "usuario")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "El nombre no puede estar vacío")
+    @Size(max = 100, message = "Máximo 100 caracteres")
+    @Column(nullable = false, length = 100)
+    private String nombreApellido;
+
+
+    @NotBlank(message = "El nombre no puede estar vacío")
+    @Size(max = 100, message = "Máximo 100 caracteres")
+    @Column(nullable = false, unique = true, length = 100)
+    private String nombrePantalla;
+
+    @Email(message = "Debe ser un correo electrónico válido")
+    @NotBlank(message = "El correo no puede estar vacío")
+    @Pattern(
+            regexp = "^[a-zA-Z0-9._%+-]+@gmail\\.com$",
+            message = "El correo debe pertenecer al dominio @gmail.com"
+    )
+    @Column(nullable = false, unique = true, length = 150)
+    private String email;
+
+    @NotBlank(message = "La contraseña es obligatoria")
+    @Size(min = 8, message = "La contraseña debe tener al menos 8 caracteres")
+    @Column(nullable = false)
+    private String password;
+
+    @DecimalMin(value = "0.0", message = "La billetera no puede tener saldo negativo")
+    @Column(nullable = false)
+    private BigDecimal billetera = BigDecimal.ZERO;
+
+    @Column(name = "cuenta_bloqueada")
+    private boolean cuentaBloqueada = false;
+
+    @Column(name = "fecha_registro")
+    private Long anioRegistro;
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/main/resources/db/migration/V1__crear_tabla_usuario.sql
@@ -0,0 +1,10 @@
+CREATE TABLE usuario (
+                         id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                         nombre_apellido VARCHAR(100) NOT NULL,
+                         nombre_pantalla VARCHAR(100) NOT NULL UNIQUE,
+                         email VARCHAR(150) NOT NULL UNIQUE,
+                         password VARCHAR(255) NOT NULL,
+                         billetera DECIMAL(19, 2) NOT NULL DEFAULT 0.00,
+                         cuenta_bloqueada BOOLEAN DEFAULT FALSE,
+                         anio_registro BIGINT
+);
